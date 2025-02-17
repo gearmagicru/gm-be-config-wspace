b6b3631b55ef027dd15ea66f8ffec0374f486aa9
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 15:23:47 +0300

    Файлы конфигурации

---
+++ config/extension.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'translator' => [
+        'locale'   => 'auto',
+        'patterns' => [
+            'text' => [
+                'basePath' => __DIR__ . '/../lang',
+                'pattern'   => 'text-%s.php'
+            ]
+        ],
+        'autoload' => ['text'],
+        'external' => [BACKEND]
+    ],
+
+    'accessRules' => [
+        // для авторизованных пользователей панели управления
+        [ // разрешение "Полный доступ" (any)
+            'allow',
+            'permission'  => 'any',
+            'controllers' => [
+                'Form' => ['view', 'update']
+            ],
+            'users' => ['@backend']
+        ],
+        [ // разрешение "Информация о расширении" (info)
+            'allow',
+            'permission'  => 'info',
+            'controllers' => ['Info'],
+            'users'       => ['@backend']
+        ],
+        [ // для всех остальных, доступа нет
+            'deny'
+        ]
+    ],
+
+    'viewManager' => [
+        'id'          => 'gm-config-wspace-{name}',
+        'useTheme'    => true,
+        'useLocalize' => true,
+        'viewMap'     => [
+            // информация о расширении
+            'info' => [
+                'viewFile'      => '//backend/extension-info.phtml', 
+                'forceLocalize' => true
+            ],
+        ]
+    ]
+];
---
+++ config/install.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Файл конфигурации установки расширения.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    'id'          => 'gm.be.config.wspace',
+    'moduleId'    => 'gm.be.config',
+    'name'        => 'Workspace',
+    'description' => 'Configuring the user workspace interface',
+    'namespace'   => 'Gm\Backend\Config\WSpace',
+    'path'        => '/gm/gm.be.config.wspace',
+    'route'       => 'wspace',
+    'locales'     => ['ru_RU', 'en_GB'],
+    'permissions' => ['any', 'info'],
+    'events'      => [],
+    'required'    => [
+        ['php', 'version' => '8.2'],
+        ['app', 'code' => 'GM MS'],
+        ['app', 'code' => 'GM CMS'],
+        ['app', 'code' => 'GM CRM'],
+        ['module', 'id' => 'gm.be.config']
+    ]
+];
