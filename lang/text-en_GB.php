c3c753d12845c738e25174694f706f3f41a93b08
Anton Tivonenko <93121899+gearmagicru@users.noreply.github.com>
2025-02-17 15:19:09 +0300

    Файлы локализации

---
+++ lang/text-en_GB.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Этот файл является частью расширения модуля веб-приложения GearMagic.
+ * 
+ * Пакет английской (британской) локализации.
+ * 
+ * @link https://gearmagic.ru
+ * @copyright Copyright (c) 2015 Веб-студия GearMagic
+ * @license https://gearmagic.ru/license/
+ */
+
+return [
+    '{name}'        => 'Workspace',
+    '{description}' => 'Configuring the user workspace interface',
+    '{permissions}' => [
+        'any' => ['Full access', 'Configuring the user workspace interface'],
+    ],
+
+    // Form: поля
+    'Main menu' => 'Main menu',
+    'Show main menu' => 'Show main menu',
+    'Tray bar' => 'Tray bar',
+    'Partition bar' => 'Partition bar',
+    'Navigator' => 'Navigator',
+    'Show panel' => 'Show panel',
+    'Panel position' => 'Panel position',
+    'Collapsible panel' => 'Collapsible panel',
+    'Split panel' => 'Split panel',
+    'Panel width' => 'Panel width, px',
+    'Right' => 'Right',
+    'Left' => 'Left',
+    'Bottom' => 'Bottom',
+    'Top' => 'Top',
+    // Form: сообщения / заголовки
+    'Save settings' => 'Сохранение настроек',
+    'Reset settings' => 'Сброс настроек',
+    // Form: сообщения / текст
+    'settings saved {0} successfully' => 'успешное сохранение настроек "<b>{0}</b>"',
+    'settings reseted {0} successfully' => 'успешный сброс настроек "<b>{0}</b>"'
+];
